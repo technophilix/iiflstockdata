aec74079e43bc107cff57d810fc3974c42dc6ce9
technophilix <technophilix2020@gmail.com>
2023-09-05 15:20:42 +0530

    some changes

--- correction.py
+++ correction.py
@@ -29,20 +29,30 @@ if response['type'] == 'success':
     output_excel = input("Enter the excel file name")
     output_excel = output_excel+ ".xlsx"
     exchangesegment = xt.EXCHANGE_NSECM
+
+    master = xt.get_master([exchangesegment])
+    print(master)
+
+
     response = xt.get_ohlc("NSECM", 22, 'Aug 22 2023 091500', 'Aug 22 2023 153000', 60)
     data = response['result']['dataReponse']
     rows = data.strip(',').split(',')
-    for row in data_list:
-        timestamp = int(row[0])  # Assuming the timestamp is in the first column
+    data_list = []
+
+    # print(rows)
+
+    for row in rows:
+        row_data = row.split('|')
+        timestamp = int(row_data[0])  # Assuming the timestamp is in the first column
         human_readable_time = datetime.utcfromtimestamp(timestamp).strftime('%Y-%m-%d %H:%M:%S')
-        row[0] = human_readable_time
-    df = pd.DataFrame([row.split('|') for row in rows])
+        row_data[0] = human_readable_time
+        data_list.append(row_data)
     column_names = [
-        'TIME', 'Open', 'High', 'Low', 'Close', 'Volume', 'OI'
+        'TIME', 'Open', 'High', 'Low', 'Close', 'Volume', 'OI',''
     ]
 
     # Assign column names to the DataFrame
-    df.columns = column_names
+    df = pd.DataFrame(data_list, columns=column_names)
     df.to_excel(output_excel, index=False)
 else:
     pass
--- readme.md
+++ readme.md
@@ -4,5 +4,5 @@ Download this repository by `git clone git@github.com:technophilix/iiflstockdata
 ![enter image description here](https://i.ibb.co/GxG8bgc/git.png)
 
 Go to the root folder and rename the `.env.example` file to the `.env` file. Then change the dummy credentials to real.  
-Open the terminal (command prompt in windows) run `pip install -r requirement.txt`  
+Open the terminal (command prompt in windows) run `pip install -r requirements.txt`  
 After installing all the dependencies, run `python main.py`
